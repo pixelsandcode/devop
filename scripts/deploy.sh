b6d10d290931b623c0b72b19a8b0438d1980fd8c
Arash Kay <arash@tectual.com.au>
2019-02-16 18:00:21 +1100

    variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+PROJECT_ID = devop-231723
+
 set -e
 
 gcloud auth activate-service-account --key-file=client-secret.json
+gcloud config set project $PROJECT_ID
 gcloud builds submit --config cloudbuild.yml .
